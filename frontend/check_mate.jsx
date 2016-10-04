116ffd4788c452faedf9d56ae692115224c001d2
Brian Manning <btreelmanning@gmail.com>
2016-10-04 18:01:18 -0400

    Finish testing session reducer and middleware

--- frontend/check_mate.jsx
+++ frontend/check_mate.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.store = configureStore(); //for testing
+  // window.store = configureStore(); //for testing
   const root = document.getElementById('root');
   ReactDOM.render(<h1>CheckMate</h1>, root);
 });
@@ -13,13 +13,13 @@ import { login, logout, signup } from './actions/session_actions';
 // window.requestLogin = requestLogin;
 // window.requestSignup = requestSignup;
 // window.requestLogout = requestLogout;
-window.login = login;
-window.logout = logout;
-window.signup = signup;
-window.goodlogin = {username: 'test', password: 'testing'};
-window.gooduser = {username: 'willywonka', password: 'pureimagination', fname: 'willy', lname: 'wonka'};
-window.baduser1 = {username: 'sirrus', password: 'art', fname: 'sirrus'};
-window.baduser2 = {username: 'sirrus', password: 'redpages'};
+// window.login = login;
+// window.logout = logout;
+// window.signup = signup;
+// window.goodlogin = {username: 'test', password: 'testing'};
+// window.gooduser = {username: 'willywonka', password: 'pureimagination', fname: 'willy', lname: 'wonka'};
+// window.baduser1 = {username: 'sirrus', password: 'art', fname: 'sirrus'};
+// window.baduser2 = {username: 'sirrus', password: 'redpages'};
 window.success = (user) => {
   console.log(user);
 };

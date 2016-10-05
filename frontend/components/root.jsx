a38022370583f232441cda42b8185dc6a69d3d57
Brian Manning <btreelmanning@gmail.com>
2016-10-05 09:03:54 -0400

    heroku hotfix

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import App from './App.jsx';
-=======
 import App from './app.jsx';
->>>>>>> usersauth
+
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 

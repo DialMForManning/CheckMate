d6e8a05628739cd58221575c7db3a323a1522554
Brian Manning <btreelmanning@gmail.com>
2016-10-05 20:32:32 -0400

    implement bootstrapping

--- frontend/check_mate.jsx
+++ frontend/check_mate.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-  window.store = store;//for testing
+  let store;
+
+  if (window.currentUser) {
+    const initialState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+    store = configureStore(initialState);
+  } else {
+    store = configureStore();
+  }
+
+  window.store = store;
+  
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
 });

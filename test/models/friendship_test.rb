5198300cd8ef27e48c772287e75be73e66f82204
Brian Manning <btreelmanning@gmail.com>
2016-10-06 11:39:53 -0400

    reannotate friendships

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -2,12 +2,12 @@
 #
 # Table name: friendships
 #
-#  id           :integer          not null, primary key
-#  requester_id :integer          not null
-#  friend_id    :integer          not null
-#  accepted     :boolean          default(TRUE), not null
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id         :integer          not null, primary key
+#  friend_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#  status     :string           not null
 #
 
 require 'test_helper'

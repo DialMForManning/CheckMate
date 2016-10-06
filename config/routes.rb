c1e6c4207b7d26891de25c468df54bd6a3460076
Brian Manning <btreelmanning@gmail.com>
2016-10-06 13:58:27 -0400

    setup friendship/friends routes and friends controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resource :friendships, only: [:create, :update, :destroy]
+    resource :friends, only: [:index]
   end
+
+  get 'api/friends', to: 'api/friends/#index', defaults: { format: :json }
 end

6d020592daee7f08fb27f93e922012e4bd5693ad
Brian Manning <btreelmanning@gmail.com>
2016-10-07 10:57:27 -0400

    tested friends api util

--- frontend/check_mate.jsx
+++ frontend/check_mate.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -17,26 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   window.store = store;
-  
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
 });
 
-import configureStore from './store/store';
 //test imports and invocations
-import { login, logout, signup } from './actions/session_actions';
-// window.requestLogin = requestLogin;
-// window.requestSignup = requestSignup;
-// window.requestLogout = requestLogout;
-window.login = login;
-window.logout = logout;
-window.signup = signup;
-window.goodlogin = {email: 'test', password: 'testing'};
-window.gooduser = {email: 'willywonka', password: 'pureimagination', fname: 'willy', lname: 'wonka'};
-window.baduser1 = {email: 'sirrus', password: 'art', fname: 'sirrus'};
-window.baduser2 = {email: 'sirrus', password: 'redpages'};
-window.success = (user) => {
-  console.log(user);
+import { fetchFriends, requestFriendship, approveFriendship, denyFriendship } from './util/friends_api_util';
+window.fetchFriends = fetchFriends;
+window.requestFriendship = requestFriendship;
+window.approveFriendship = approveFriendship;
+window.denyFriendship = denyFriendship;
+
+window.success = (friends) => {
+  console.log(friends);
 };
 
 window.errors = (errors) => {console.log(errors);};

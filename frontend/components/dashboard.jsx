d0a1f71a4a2963fb0ab7f6e7616a0f18f7ac39b0
Brian Manning <btreelmanning@gmail.com>
2016-10-10 14:07:26 -0400

    display summary instead of expenses on dashboard and update css

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -3,6 +3,7 @@ import NavbarContainer from './navbar/navbar_container';
 import FriendsPaneContainer from './friends_pane/friends_pane_container';
 import ExpensesPane from './expenses_pane/expenses_pane';
 import DetailPane from './detail_pane/detail_pane';
+import SummaryPane from './summary_pane';
 
 const Dashboard = () => {
   return(
@@ -10,7 +11,7 @@ const Dashboard = () => {
       <NavbarContainer />
       <article className='dashboard group'>
         <FriendsPaneContainer />
-        <ExpensesPane />
+        <SummaryPane />
         <DetailPane />
       </article>
     </main>
---
+++ frontend/components/summary_pane.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SummaryPane = () => {
+  return (
+    <content className="summary_pane">
+      <h1>Summary Pane</h1>
+    </content>
+  );
+};
+
+export default SummaryPane;

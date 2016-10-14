5642a2cc4201a0797153aa64545d8be8233316ec
Brian Manning <btreelmanning@gmail.com>
2016-10-13 20:27:24 -0400

    index comments by friend_id in search query

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
     resources :expenses, only: [:create, :update, :destroy]
     resources :transactions, only: [:destroy]
     resources :expenses do
-      resources :comments, only: [:create, :index]
+      resources :comments, only: [:create]
     end
-    resources :comments, only: [:update, :destroy]
+    resources :comments, only: [:update, :index, :destroy]
   end
 
   get 'api/friends/:id/transactions', to: 'api/transactions#index', defaults: { format: :json }
